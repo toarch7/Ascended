43374269171aedf9a15915902988610898d3513f
toncho_ <toncho.dev4@gmail.com>
2022-09-30 13:38:45 +0300

    a10 fix

--- a_scripts/ascensions/10_rare_room_charge.lua
+++ a_scripts/ascensions/10_rare_room_charge.lua
@@ -3,34 +3,30 @@ AscensionDesc = "Room may not gain you charge"
 local mod = Ascended
 
 AscensionInit = function()
-	mod:AddAscensionCallback("PostRoomAward", function()
-		if mod.postSpawnCleanAward then
-			local c = mod.Data.run.roomsCleared
+	mod:AddAscensionCallback("PostRoomAward", function(p)
+		local c = mod.Data.run.roomsCleared
 
-			if c == nil then
-				c = 0
-			end
+		if c == nil then
+			c = 0
+		end
+		
+		if c % 3 == 0 then
+			local slots = { ActiveSlot.SLOT_PRIMARY, ActiveSlot.SLOT_SECONDARY, ActiveSlot.SLOT_POCKET, ActiveSlot.SLOT_POCKET2 }
 			
-			if mod.Data.roomsCleared % 3 == 0 then
-				local slots = { ActiveSlot.SLOT_PRIMARY, ActiveSlot.SLOT_SECONDARY, ActiveSlot.SLOT_POCKET, ActiveSlot.SLOT_POCKET2 }
+			for _, v in pairs(slots) do
+				local item = p:GetActiveItem(v)
 				
-				for _, v in pairs(slots) do
-					local item = p:GetActiveItem(v)
+				if item ~= 0 then
+					local cur = p:GetActiveCharge(v)
+					local m = Isaac.GetItemConfig():GetCollectible(item).MaxCharges
 					
-					if item ~= 0 then
-						local cur = p:GetActiveCharge(v)
-						local m = Isaac.GetItemConfig():GetCollectible(item).MaxCharges
-						
-						if cur > 0 and cur < m then
-							p:SetActiveCharge(cur - 1, v)
-						end
+					if cur > 0 and cur < m then
+						p:SetActiveCharge(cur - 1, v)
 					end
 				end
 			end
-
-			mod.postSpawnCleanAward = false
-
-			mod.Data.run.roomsCleared = c + 1
 		end
+
+		mod.Data.run.roomsCleared = c + 1
 	end)
 end
